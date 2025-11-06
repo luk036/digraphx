2248df4b2bd83f7bcec9a276c153bcc7d5de3eb3
Wai-Shing Luk <luk036@gmail.com>
2025-11-06 21:47:13 +0800

    fix: run black to fix linting errors

--- tests/test_min_parmetric_q.py
+++ tests/test_min_parmetric_q.py
@@ -28,9 +28,9 @@ def test_min_parametric_q() -> None:
         "a2": {"a1": {"cost": 1, "time": 1}, "a0": {"cost": 2, "time": 1}},
     }
     dist: Dict[str, float] = {vtx: float("inf") for vtx in digraph}
-    solver: MinParametricSolver[str, Dict[str, int], Fraction, float] = (
-        MinParametricSolver(digraph, MyAPI())
-    )
+    solver: MinParametricSolver[
+        str, Dict[str, int], Fraction, float
+    ] = MinParametricSolver(digraph, MyAPI())
     ratio, cycle = solver.run(dist, Fraction(0), lambda D, d: D > d)
     assert ratio == Fraction(0, 1)
     assert cycle == []
